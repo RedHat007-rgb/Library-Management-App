cc40250fb6597d3f44df7a728871333a8eb305a2
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-04-07 16:07:37 +0530

    Updated with useRef

--- react-frontend/src/Components/AddBook.jsx
+++ react-frontend/src/Components/AddBook.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const AddBook = () => {
+  const inputRef=useRef(null);
+
+ useEffect(()=>{
+  inputRef.current.focus();
+ },[])
+
+ 
   return (
     <div>
-      Add Book
+      <form>
+        <label>
+          Title:
+          <input ref={inputRef} placeholder='Enter the Title:'></input>
+        </label>
+        <label>
+          Author:
+          <input placeholder='Enter the Author:'></input>
+        </label>
+        <label>
+          ISBN:
+          <input placeholder='Enter the ISBN:'></input>
+        </label>
+        <button >Add Book</button>
+      </form>
     </div>
   )
 }
